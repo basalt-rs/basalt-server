c1cd0c8a513f86289f33f6934617de19e490356a
JingusJohn <branchjohnchristopher@gmail.com>
2025-01-31 15:31:14 -0600

    using anyhow for build.rs

--- build.rs
+++ build.rs
@@ -1,28 +1,24 @@
+use anyhow::Context;
+
 const INITIAL_DATA_PATH: &str = "./initial_data.db";
 
 #[tokio::main]
-pub async fn main() -> Result<(), String> {
+pub async fn main() -> anyhow::Result<()> {
     println!("cargo::rerun-if-changed=migration.sql");
     std::env::set_var("DATABASE_URL", "sqlite:initial_data.db");
 
-    if tokio::fs::try_exists(INITIAL_DATA_PATH)
+    tokio::fs::File::create(INITIAL_DATA_PATH)
         .await
-        .expect("Failed to check existence of initial_data.db")
-    {
-        tokio::fs::remove_file(INITIAL_DATA_PATH).await.unwrap()
-    }
-    tokio::fs::File::create_new(INITIAL_DATA_PATH)
-        .await
-        .expect("failed to create db");
+        .context("Failed to create db")?;
 
     let db = sqlx::sqlite::SqlitePool::connect("sqlite:initial_data.db")
         .await
-        .unwrap();
+        .context("Failed to create database layer")?;
 
     sqlx::raw_sql(include_str!("./migration.sql"))
         .execute(&db)
         .await
-        .unwrap();
+        .context("Failed to execute migration")?;
 
     Ok(())
 }
