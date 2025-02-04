8b42b589d968e74207295ce0360a2bdada46a7b1
funnyboy-roks <funnyboyroks@gmail.com>
2025-02-04 13:30:01 -0600

    break apart `ws` module

--- src/services/ws/mod.rs
+++ src/services/ws/mod.rs
@@ -10,6 +10,7 @@ use tracing::{debug, trace};
 use utoipa_axum::{router::OpenApiRouter, routes};
 
 use crate::server::AppState;
+pub mod connect;
 
 #[derive(Debug, Clone, Eq, PartialEq, PartialOrd, Ord, Hash)]
 pub enum ConnectionKind {
@@ -20,125 +21,6 @@ pub enum ConnectionKind {
 #[derive(Debug, Clone)]
 pub struct ConnectedClient {
     pub send: mpsc::UnboundedSender<WebSocketSend>,
-}
-
-pub mod connect {
-    use std::{net::SocketAddr, sync::Arc};
-
-    use anyhow::{bail, Context};
-    use axum::{
-        extract::{
-            ws::{Message, WebSocket},
-            ConnectInfo, State, WebSocketUpgrade,
-        },
-        response::Response,
-    };
-    use serde::Serialize;
-    use tokio::sync::mpsc;
-    use tracing::{debug, error, trace, warn};
-    use utoipa::ToSchema;
-
-    use super::{ConnectedClient, ConnectionKind};
-    use crate::{server::AppState, services::ws::WebSocketRecv};
-
-    #[derive(Serialize, ToSchema)]
-    pub struct ConnectionResponse(u32);
-
-    #[axum::debug_handler]
-    #[utoipa::path(get, path = "/", responses((status = OK, body = ConnectionResponse, content_type = "application/json")))]
-    pub async fn handler(
-        ws: WebSocketUpgrade,
-        ConnectInfo(addr): ConnectInfo<SocketAddr>,
-        State(state): State<Arc<AppState>>,
-    ) -> Response {
-        // TODO: This should be associated with a user once we have authentication setup.
-        let who = ConnectionKind::Leaderboard { addr };
-
-        trace!(?who, "Client connect");
-        ws.on_upgrade(move |ws| async move {
-            if let Err(e) = handle_socket(ws, who.clone(), state).await {
-                error!(?who, ?e, "Error handling websocket connection");
-            }
-        })
-    }
-
-    async fn handle_message(
-        msg: Message,
-        ws: &mut WebSocket,
-        who: &ConnectionKind,
-        state: &Arc<AppState>,
-    ) -> anyhow::Result<()> {
-        match msg {
-            Message::Text(bytes) => match serde_json::from_str::<WebSocketRecv>(bytes.as_str()) {
-                Ok(msg) => {
-                    trace!(?msg, "Receiving websocket message");
-                    msg.handle(who, state)
-                        .await
-                        .context("handling websocket message")?;
-                }
-                Err(error) => {
-                    debug!(?error, "Ignoring invalid websocket message");
-                }
-            },
-            Message::Binary(_) => {
-                warn!("Ignoring unexpected binary message");
-            }
-            Message::Ping(bytes) => {
-                ws.send(Message::Pong(bytes)).await?;
-            }
-            Message::Pong(_) => {}
-            Message::Close(_) => {
-                trace!("Close message received");
-            }
-        }
-        Ok(())
-    }
-
-    #[tracing::instrument(skip(ws, state))]
-    async fn handle_socket(
-        mut ws: WebSocket,
-        who: ConnectionKind,
-        state: Arc<AppState>,
-    ) -> anyhow::Result<()> {
-        let (tx, mut rx) = mpsc::unbounded_channel();
-        state
-            .active_connections
-            .insert(who.clone(), ConnectedClient { send: tx });
-        if ws.send(Message::text("ping")).await.is_ok() {
-            trace!("Send ping");
-        } else {
-            bail!("Could not send ping!");
-        }
-
-        loop {
-            tokio::select! {
-                msg = rx.recv() => match msg {
-                    None => {
-                        // Connection is closed.
-                        trace!("Connection closed");
-                        return Ok(());
-                    },
-                    Some(msg) => {
-                        trace!(?msg, "Sending message on websocket");
-                        ws.send(Message::text(serde_json::to_string(&msg)?)).await?;
-                    }
-                },
-                msg = ws.recv() => match msg {
-                    None => {
-                        return Ok(());
-                    },
-                    Some(Err(error)) => {
-                        debug!(?error, "Error while waiting for websocket message");
-                        return Ok(());
-                    },
-                    Some(Ok(msg)) => {
-                        trace!(?msg, "recv msg");
-                        handle_message(msg, &mut ws, &who, &state).await?;
-                    }
-                }
-            }
-        }
-    }
 }
 
 /// A message that is recieved from the websocket
