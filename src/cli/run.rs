e47d8fdd2522468d99e2f567b0af1a41a6a78183
Funny <funnyboyroks@gmail.com>
2025-02-12 00:11:41 -0600

    Use port from config (#8)
    * use port from config
    
    * allow port through --port
    
    * s/packet/config and default to "basalt.toml" for config
    
    * properly print packet parse error
    
    * flatten error matching

--- src/cli/run.rs
+++ src/cli/run.rs
@@ -12,11 +12,17 @@ use crate::{
 
 #[derive(Parser, Debug)]
 pub struct RunArgs {
-    #[arg()]
-    port: u16,
+    /// Port on which to host the server.  Defaults to the port specified in the config file if
+    /// omitted.
+    #[arg(long, short)]
+    port: Option<u16>,
+    /// Name of the competition.  Used to determine location for data folder.  Defaults to a random
+    /// string if omitted.
     #[arg(long, short)]
     name: Option<String>,
-    packet: PathBuf,
+    /// Path to the configuration to use to host the server.
+    #[arg(default_value = "basalt.toml")]
+    config: PathBuf,
 }
 
 fn default_name() -> String {
@@ -30,21 +36,26 @@ fn default_name() -> String {
 pub async fn handle(args: RunArgs) -> anyhow::Result<()> {
     info!("Parsing packet configurations");
 
-    let file = tokio::fs::File::open(&args.packet)
+    let file = tokio::fs::File::open(&args.config)
         .await
         .context("Opening packet file")?;
 
     let mut file = tokio::io::BufReader::new(file);
 
     let file_name = args
-        .packet
+        .config
         .file_name()
         .expect("call to File::open would fail if this does")
         .to_str();
 
-    let config = bedrock::Config::read_async(&mut file, file_name)
-        .await
-        .context("Failed to parse configurations")?;
+    let config = match bedrock::Config::read_async(&mut file, file_name).await {
+        Ok(config) => config,
+        Err(err @ bedrock::ConfigReadError::ReadError(_)) => Err(err)?,
+        Err(bedrock::ConfigReadError::MalformedData(err)) => {
+            eprintln!("{:?}", err);
+            anyhow::bail!("parsing config");
+        }
+    };
 
     let name = &args.name.unwrap_or_else(default_name);
     info!(name, "Creating Sqlite layer");
@@ -52,7 +63,9 @@ pub async fn handle(args: RunArgs) -> anyhow::Result<()> {
         .await
         .context("Creating Sqlite Layer")?;
 
-    let addr: SocketAddr = format!("[::]:{}", args.port).parse().unwrap();
+    let addr: SocketAddr = format!("[::]:{}", args.port.unwrap_or(config.port))
+        .parse()
+        .unwrap();
     info!(?addr, "Serving via HTTP");
 
     let listener = tokio::net::TcpListener::bind(addr).await?;
