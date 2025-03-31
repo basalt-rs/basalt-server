60e39f9cf160d666cc8ec4c280acedddfbd55e13
JingusJohn <branchjohnchristopher@gmail.com>
2025-03-31 17:25:31 -0500

    implemented routes (untested)

--- basalt-server-lib/src/services/clock.rs
+++ basalt-server-lib/src/services/clock.rs
@@ -2,17 +2,10 @@ use std::{sync::Arc, time::Duration};
 
 use axum::{extract::State, http::StatusCode, Json};
 use bedrock::{Game, PointsSettings};
-use tracing::{debug, trace};
+use tracing::trace;
 use utoipa_axum::{router::OpenApiRouter, routes};
 
-use crate::{
-    extractors::auth::AuthUser,
-    repositories::{
-        self,
-        users::{Role, User, UserLogin},
-    },
-    server::AppState,
-};
+use crate::{extractors::auth::AuthUser, server::AppState};
 
 #[derive(serde::Deserialize, utoipa::ToSchema)]
 #[serde(untagged)]
@@ -21,7 +14,7 @@ pub enum UpdateClockRequest {
 }
 
 #[derive(serde::Serialize, utoipa::ToSchema)]
-struct UpdateClockResponse {
+struct ClockStatusResponse {
     is_paused: bool,
     time_left_seconds: u64,
 }
@@ -29,17 +22,17 @@ struct UpdateClockResponse {
 #[axum::debug_handler]
 #[utoipa::path(
     patch,
-    path="/", tag="auth", request_body=UpdateClockRequest,
+    path="/", tag="clock", request_body=UpdateClockRequest,
     responses(
-        (status=OK, body=UpdateClockResponse, description="Game is paused"),
+        (status=OK, body=ClockStatusResponse, description="Game is paused"),
         (status=UNAUTHORIZED, description="Insufficient permissions"),
     )
 )]
-async fn update_clock(
+async fn patch_clock(
     State(state): State<Arc<AppState>>,
     user: AuthUser,
     Json(update): Json<UpdateClockRequest>,
-) -> Result<Json<UpdateClockResponse>, StatusCode> {
+) -> Result<Json<ClockStatusResponse>, StatusCode> {
     trace!(user.user.username, "attempt to pause server");
 
     let time_limit = match state.config.game {
@@ -60,11 +53,11 @@ async fn update_clock(
     }
 
     match clock.current_time() {
-        Ok(current_time) => Ok(Json(UpdateClockResponse {
+        Ok(current_time) => Ok(Json(ClockStatusResponse {
             is_paused: current_time.paused,
             time_left_seconds: current_time.time_left(time_limit).as_secs(),
         })),
-        Err(_) => Ok(Json(UpdateClockResponse {
+        Err(_) => Ok(Json(ClockStatusResponse {
             is_paused: false,
             time_left_seconds: Duration::from_secs(0).as_secs(),
         })),
@@ -73,43 +66,38 @@ async fn update_clock(
 
 #[axum::debug_handler]
 #[utoipa::path(
-    post,
-    path="/logout", tag="auth",
-    responses(
-        (status=OK, description="User has been logged out"),
-        (status=401, description="User was not logged in"),
-    )
-)]
-async fn logout(State(state): State<Arc<AppState>>, user: AuthUser) -> Result<(), StatusCode> {
-    debug!(user.user.username, "logout");
-    let db = state.db.read().await;
-
-    repositories::session::close_session(&db, &user.session_id)
-        .await
-        .unwrap();
-
-    Ok(())
-}
-
-#[axum::debug_handler]
-#[utoipa::path(
     get,
-    path="/me", tag="auth",
-    description="Get information about the current user",
+    path="/", tag="clock",
     responses(
-        (status=OK, body=User, description="User is signed in"),
-        (status=401, description="Auth token is expired"),
+        (status=OK, body=ClockStatusResponse, description="Information about the clock"),
     )
 )]
-async fn me(State(_state): State<Arc<AppState>>, user: AuthUser) -> Result<Json<User>, StatusCode> {
-    Ok(Json(user.user))
+async fn get_clock(
+    State(state): State<Arc<AppState>>,
+) -> Result<Json<ClockStatusResponse>, StatusCode> {
+    let time_limit = match state.config.game {
+        // TODO: When time_limit is made public, update this
+        Game::Points(PointsSettings { .. }) => Duration::from_secs(60 * 75),
+        // TODO: When other modes are supported, provide correct values
+        _ => Duration::from_secs(60 * 75),
+    };
+    let clock = state.clock.read().await;
+    match clock.current_time() {
+        Ok(current_time) => Ok(Json(ClockStatusResponse {
+            is_paused: current_time.paused,
+            time_left_seconds: current_time.time_left(time_limit).as_secs(),
+        })),
+        Err(_) => Ok(Json(ClockStatusResponse {
+            is_paused: false,
+            time_left_seconds: Duration::from_secs(0).as_secs(),
+        })),
+    }
 }
 
 pub fn router() -> OpenApiRouter<Arc<AppState>> {
     OpenApiRouter::new()
-        .routes(routes!(login))
-        .routes(routes!(logout))
-        .routes(routes!(me))
+        .routes(routes!(get_clock))
+        .routes(routes!(patch_clock))
 }
 
 pub fn service() -> axum::Router<Arc<AppState>> {
